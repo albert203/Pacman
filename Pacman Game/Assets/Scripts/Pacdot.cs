745ef6c2e727fb2100b9e64951b7146ff358f1ac
albert203 <albert.jordaan@outlook.com>
2019-06-27 04:06:45 +1200

    made new pacdots, destroyable

---
+++ Pacman Game/Assets/Scripts/Pacdot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour
+{
+    
+    void OnTriggerEnter2D(Collider2D entitycollision)
+    {
+        if (entitycollision.name == "pacman")
+        {
+            Destroy(gameObject);
+        }
+    }
+        //Note: if we wanted to implement a highscore in our game, then this would be the place to increase it.
+       
+}
