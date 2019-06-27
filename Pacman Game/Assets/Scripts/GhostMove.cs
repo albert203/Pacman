d9a3d9f87e07f3421a1ba027adffb3d7c0ff264c
Albert Jordaan <albert.jordaan@outlook.com>
2019-06-28 01:42:40 +1200

    updates

--- Pacman Game/Assets/Scripts/GhostMove.cs
+++ Pacman Game/Assets/Scripts/GhostMove.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine;
+using System.Collections;
+
 public class GhostMove : MonoBehaviour
 {
     public Transform[] waypoints;
     int cur = 0;
+
     public float speed = 0.3f;
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+
         // Waypoint not reached yet? then move closer
         if (transform.position != waypoints[cur].position)
         {
@@ -26,10 +30,12 @@ public class GhostMove : MonoBehaviour
         Vector2 dir = waypoints[cur].position - transform.position;
         GetComponent<Animator>().SetFloat("DirX", dir.x);
         GetComponent<Animator>().SetFloat("DirY", dir.y);
+
     }
     void OnTriggerEnter2D(Collider2D co)
     {
         if (co.name == "pacman")
             Destroy(co.gameObject);
     }
+
 }
